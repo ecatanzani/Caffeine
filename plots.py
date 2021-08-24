b7f8132ebdae031a24bd1d804ba9fba34521a51b
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-08-24 15:16:49 +0200

    Major bug fix in plots

--- plots.py
+++ plots.py
@@ -7,8 +7,9 @@ def get_slope_color(values: list, reverse=False) -> list:
     green = '#3DF900'
     red = '#F90004'
     yellow = '#F9E300'
+    gray = '#606060'
     
-    colors = [green]
+    colors = [gray]
     last_value = values[0]
 
     for elm in values[1:]:
@@ -78,6 +79,7 @@ def macro_plain(measurements: dict, style_color: str):
     })
 
     weight_chart = alt.Chart(time_data).mark_line(clip=True).mark_area(
+        opacity = 0.3,
         line={'color': style_color},
         color=alt.Gradient(
             gradient='linear',
@@ -94,16 +96,21 @@ def macro_plain(measurements: dict, style_color: str):
             alt.Y('Weight:Q')
         ).interactive()
 
-    weight_circles = alt.Chart(time_data).mark_point(
-        filled=True,
-        size=100
-    ).encode(
-        x='Date:T',
-        y='Weight:Q',
-        color = alt.Color('weight_color_bullets', legend=None, scale=None)
-    ).interactive()
+    #weight_circles = alt.Chart(time_data).mark_point(
+    #    filled=True,
+    #    size=100
+    #).encode(
+    #    x='Date:T',
+    #    y='Weight:Q',
+    #    color = alt.Color('weight_color_bullets', legend=None, scale=None)
+    #).interactive()
     
-    points_macro = alt.Chart(time_data).transform_fold(
+    orange = '#ff7f0e'
+    green = '#2ca02c'
+    sky = '#17becf'
+    color_range = [orange, green, sky]
+
+    macro = alt.Chart(time_data).transform_fold(
         ['Macro1', 'Macro2', 'Macro3'],
         as_=["macros", "value"],
     ).mark_line(
@@ -111,12 +118,11 @@ def macro_plain(measurements: dict, style_color: str):
         opacity=0.7
     ).encode(
         x='Date:T',
-        y='value:Q',
-        color='macros:N'
-        #color=alt.Color('macros', scale=alt.Scale(domain=['Weight', 'Macro1', 'Macro2', 'Macro3'], range=['red', 'steelblue', 'chartreuse', '#F4D03F'], type="ordinal"))
+        y=alt.Y("value:Q"),
+        color=alt.Color('macros:N', scale=alt.Scale(domain=['Macro1', 'Macro2', 'Macro3'], range=color_range, type="ordinal"))
     ).properties(title='Weight and Macros Time Evolution').interactive()
     
-    st.write(weight_chart + weight_circles + points_macro)
+    st.write(macro + weight_chart)
 
 def macro_stack(measurements: dict, style_color: str):
 
@@ -128,23 +134,46 @@ def macro_stack(measurements: dict, style_color: str):
         'Macro3': measurements['macro3'] 
     })
 
-    stack = alt.Chart(time_data).transform_fold(
-        ['Weight', 'Macro1', 'Macro2', 'Macro3'],
-        as_=["macros", "value"],
-    ).mark_area().encode(
-    alt.X('Date:T'),
-    alt.Y('sum(value):Q', stack='center', axis=None),
-    alt.Color('macros:N')
-    
-    ).interactive()
+    orange = '#ff7f0e'
+    green = '#2ca02c'
+    sky = '#17becf'
+    color_range = [orange, green, sky]
 
     stack = alt.Chart(time_data).transform_fold(
         ['Macro1', 'Macro2', 'Macro3'],
         as_=["macros", "value"],
     ).mark_area(opacity=0.3).encode(
         x="Date:T",
-        y=alt.Y("value:Q", stack=None),
-        color="macros:N"
+        y=alt.Y("value:Q", stack=True),
+        color=alt.Color('macros:N', scale=alt.Scale(domain=['Macro1', 'Macro2', 'Macro3'], range=color_range, type="ordinal"))
     ).properties(title='Macros - Stack Wiew').interactive()
 
     st.write(stack)
+
+def single_histo_with_mean(measurements: dict, yvar: str, yvar_name: str, plot_title: str, style_color: str):
+
+    time_data = pd.DataFrame({yvar_name: measurements[yvar]})
+
+    bar = alt.Chart(time_data).mark_bar(
+        opacity=0.8,
+        color=alt.Gradient(
+            gradient='linear',
+            stops=[
+                alt.GradientStop(color='white', offset=0), 
+                alt.GradientStop(color=style_color, offset=1)],
+                x1=1,
+                x2=1,
+                y1=1,
+                y2=0
+        )
+        ).encode(
+            x=alt.X(f"{yvar_name}:Q", bin=True),
+            y='count()'
+        ).properties(title=plot_title).interactive()
+
+    rule = alt.Chart(time_data).mark_rule(color='red').encode(
+        x=f"mean({yvar_name}):Q",
+        size=alt.value(5)
+    )
+
+    st.write(bar+rule)
